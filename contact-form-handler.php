a74d0c5d69a07732890cf4a3b2c47bc54e0334e2
viraldkt <thakkarviral1@yahoo.com>
2020-02-28 13:15:13 +0530

    a

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,14 +1,14 @@
 <?php
 
-$name = $_post['name'];
-$visitor_email = $_post['email']
-$visitor_phone = $_post['number']
-$message = $_post['message']
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$visitor_phone = $_POST['number'];
+$message = $_POST['message'];
 
 
-$email_form = 'thakkarviral1@yahoo.com'
+$email_from = 'parlepolicemitra@gmail.com';
 
-$email_subject = 'new form submission'
+$email_subject = 'new form submission';
 
 $email_body = "User Name: $name.\n".
         "User Email: $visitor_email.\n".
@@ -16,13 +16,13 @@ $email_body = "User Name: $name.\n".
             "User Message: $message.\n".
 
 
-$to = "thakkarviral1@yahoo.com";
+$to = "parlepolicemitra@gmail.com";
 
 $headers = "From: $email_from \r\n";
 
 $headers = "Reply-to: $visitor_email \r\n";
 
-mail($to,$email_subject,,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers);
 
-header ("Location: contact-us.html");
+header ("Location: index.html");
 ?>
